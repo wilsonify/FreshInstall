1179b9a655199b6b6d70d0f579a0773c2f4e3a47
Thomas Wilson <tom.andrew.wilson@gmail.com>
2018-09-03 13:04:42 -0500

    make more atomic

--- atomic_install_java.sh
+++ atomic_install_java.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-sudo R CMD javareconf
+sudo apt-get install openjdk-8-jdk
+LINE='JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin"'
+FILE='/etc/environment'
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE" #check for line in file. If missing, add it.
+source $FILE
+sudo R CMD javareconf 
