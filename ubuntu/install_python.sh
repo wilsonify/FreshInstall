32653a175cd75e8d8461c240c34d039994d1853e
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-01-05 16:49:43 -0500

    update python install

---
+++ ubuntu/install_python.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo apt-get install python3-dev python3-distutils python3-pip
+sudo -H python3 -m pip install --upgrade pip
+sudo python3 -m pip install requests bs4 matplotlib scipy numpy pandas seaborn
