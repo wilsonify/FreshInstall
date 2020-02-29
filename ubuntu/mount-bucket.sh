a6efd5ead2b59e45127457ae4286b2f7d36c0b37
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-02-29 13:30:01 -0500

    scripts for mounting s3fs

---
+++ ubuntu/mount-bucket.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+until [ "$(/usr/bin/docker inspect -f {{.State.Running}} minioblobstore)" == "true" ];
+do sleep 0.5;
+done;
+
+s3fs bucket /home/thom/bucket -o passwd_file=/home/thom/s3cred,use_path_request_style,url=http://192.168.1.125:9000,uid=1000,gid=100
