e64592693bbcb75255f42bfd5c7588dd081eec04
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-02-29 13:30:33 -0500

    use localhost

--- ubuntu/mount-bucket.sh
+++ ubuntu/mount-bucket.sh
@@ -3,4 +3,4 @@ until [ "$(/usr/bin/docker inspect -f {{.State.Running}} minioblobstore)" == "tr
 do sleep 0.5;
 done;
 
-s3fs bucket /home/thom/bucket -o passwd_file=/home/thom/s3cred,use_path_request_style,url=http://192.168.1.125:9000,uid=1000,gid=100
+s3fs bucket /home/thom/bucket -o passwd_file=/home/thom/s3cred,use_path_request_style,url=http://localhost:9000,uid=1000,gid=100
