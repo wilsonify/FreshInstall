e1f38ea739912a251817d8439cc3e0e717124d92
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-02-29 13:31:16 -0500

    comment out wait for docker logic

--- ubuntu/mount-bucket.sh
+++ ubuntu/mount-bucket.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-until [ "$(/usr/bin/docker inspect -f {{.State.Running}} minioblobstore)" == "true" ];
-do sleep 0.5;
-done;
+# to wait for docker, uncomment the next line
+#until [ "$(/usr/bin/docker inspect -f {{.State.Running}} minioblobstore)" == "true" ]; do sleep 0.5; done;
 
 s3fs bucket /home/thom/bucket -o passwd_file=/home/thom/s3cred,use_path_request_style,url=http://localhost:9000,uid=1000,gid=100
