832b5f6fc65ca8d2aaa6279ba1d2af16a17d1ecd
wilsonify <tom.a.wilson@gmail.com>
2020-03-14 14:43:31 -0400

    kodi install script

---
+++ ubuntu/install_kodi.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:team-xbmc/ppa
+sudo apt-get update
+sudo apt-get install kodi
+sudo apt-get install \
+kodi-pvr-argustv \
+kodi-pvr-demo \
+kodi-pvr-dvbviewer \
+kodi-pvr-iptvsimple \
+kodi-pvr-mediaportal-tvserver \
+kodi-pvr-mythtv \
+kodi-pvr-nextpvr \
+kodi-pvr-njoy \
+kodi-pvr-hts \
+kodi-pvr-vdr-vnsi \
+kodi-pvr-vuplus \
+kodi-pvr-dvblink \
+kodi-pvr-filmon \
+kodi-pvr-hdhomerun \
+kodi-pvr-pctv \
+kodi-pvr-stalker \
+kodi-pvr-vbox \
+kodi-pvr-wmc \
+kodi-audiodecoder-modplug \
+kodi-audiodecoder-nosefart \
+kodi-audiodecoder-sidplay \
+kodi-audiodecoder-snesapu \
+kodi-audiodecoder-stsound \
+kodi-audiodecoder-timidity \
+kodi-audiodecoder-vgmstream
