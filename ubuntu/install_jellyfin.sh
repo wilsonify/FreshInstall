3f8d8e4e1fdb5bcd6c23fa13448c0df112354b13
wilsonify <tom.a.wilson@gmail.com>
2020-03-15 22:53:08 -0400

    create install script for jellyfin

---
+++ ubuntu/install_jellyfin.sh
@@ -0,0 +1,6 @@
+sudo apt install apt-transport-https
+wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo apt-key add -
+echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+sudo apt update
+sudo apt install jellyfin
+sudo systemctl enable jellyfin.service
