c45d4f03a2424aca521d9b2b8f2c2f995c81fe39
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-04-04 15:42:24 -0400

    add script to install dotnet core

---
+++ ubuntu/install_dotnet_core.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-3.1
+sudo apt-get install aspnetcore-runtime-3.1
