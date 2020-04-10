6fc6dc029dcb0bd5244f01c156a551a47b729396
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-04-10 15:54:04 -0400

    replace s3fs with goofys

--- ubuntu/mount-bucket.sh
+++ ubuntu/mount-bucket.sh
@@ -2,4 +2,5 @@
 # to wait for docker, uncomment the next line
 #until [ "$(/usr/bin/docker inspect -f {{.State.Running}} minioblobstore)" == "true" ]; do sleep 0.5; done;
 
-s3fs bucket /home/thom/bucket -o passwd_file=/home/thom/s3cred,use_path_request_style,url=http://localhost:9000,uid=1000,gid=100
+#s3fs bucket /home/thom/bucket -o passwd_file=/home/thom/s3cred,use_path_request_style,url=http://localhost:9000,allow_other,uid=1000,gid=1000
+goofys --endpoint http://localhost:9000 bucket /home/thom/bucket
