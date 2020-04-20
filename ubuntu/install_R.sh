54405a80b8f2819ddc3f4e757931ad708fa43068
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-04-19 21:52:40 -0400

    update r

--- ubuntu/install_R.sh
+++ ubuntu/install_R.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-sudo apt-get install r-base curl libcurl4-openssl-dev libxml2-dev libssl-dev 
+gpg --keyserver keyserver.ubuntu.com --recv-key E298A3A825C0D65DFD57CBB651716619E084DAB9
+gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | sudo apt-key add -
+sudo apt-add-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu/ bionic-cran35/'
+sudo apt-get update
+sudo apt-get install r-base-dev curl libcurl4-openssl-dev libxml2-dev libssl-dev
 
