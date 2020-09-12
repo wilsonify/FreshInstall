379d6c40f9e4ade90903aaef82f123420ea6497d
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-09-12 11:29:56 -0400

    download go from golang.org add to path if not already on path

--- ubuntu/install_go.sh
+++ ubuntu/install_go.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-sudo apt install golang-go
+GO_TAR_FILE=go1.15.2.linux-amd64.tar.gz
+PATH_ADDITION='export PATH=$PATH:/usr/local/go/bin'
+
+wget "https://golang.org/dl/$GO_TAR_FILE"
+sudo tar -C /usr/local -xzf $GO_TAR_FILE
+
+export $PATH_ADDITION
+if grep -qF "$PATH_ADDITION" ~/.bashrc; then
+  echo "$PATH_ADDITION is already in .bashrc"
+else
+  echo "adding $PATH_ADDITION to .bashrc"
+  echo "$PATH_ADDITION" >>~/.bashrc
+fi
+
+source ~/.bashrc
