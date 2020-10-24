2ecb490b4c16918407f9dd9d3ef41ff6284db571
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-10-24 11:27:06 -0400

    add calibre to mount script

--- ubuntu/mount-bucket.sh
+++ ubuntu/mount-bucket.sh
@@ -4,3 +4,4 @@
 
 #s3fs bucket /home/thom/bucket -o passwd_file=/home/thom/s3cred,use_path_request_style,url=http://localhost:9000,allow_other,uid=1000,gid=1000
 goofys --endpoint http://localhost:9000 bucket /home/thom/bucket
+goofys --endpoint http://localhost:9000 calibre /home/thom/calibre
