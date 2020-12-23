12ae56b746c27ba9ba264c8861bbb62a2d1d9409
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-12-23 09:24:27 -0500

    add docker-compose install

--- ubuntu/install_docker.sh
+++ ubuntu/install_docker.sh
@@ -16,3 +16,8 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+echo "install compose"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
