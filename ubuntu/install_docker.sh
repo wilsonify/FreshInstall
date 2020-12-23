b8dae735f8103af34544586004bc1e415224b30b
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-12-23 08:59:36 -0500

    create docker install script

---
+++ ubuntu/install_docker.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+
+sudo apt-get install \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg-agent \
+software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 9DC858229FC7DD38854AE2D88D81803C0EBFCD88
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
