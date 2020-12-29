5d300780c01efa76773b98b60f25c5e899a0df9e
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-12-29 14:09:53 -0500

    simplify by using run file

--- ubuntu/tensorflow/nvidia_cuda_10.2_install.sh
+++ ubuntu/tensorflow/nvidia_cuda_10.2_install.sh
@@ -1,2 +1,2 @@
 wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
-sudo sh cuda_10.2.89_440.33.01_linux.run --toolkit
+sudo ./cuda_10.2.89_440.33.01_linux.run --toolkit
