dc64a7a450695cef185f7437355bf2900dc13f5d
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-12-29 12:28:04 -0500

    add apt purge

--- ubuntu/tensorflow/remove_all_nvidia.sh
+++ ubuntu/tensorflow/remove_all_nvidia.sh
@@ -1,3 +1,8 @@
 sudo apt remove nvidia*
+sudo apt-get autoremove
+
+sudo apt-get purge *nvidia*
+sudo apt-get autoremove
+
 sudo dpkg -P "$(dpkg -l | grep nvidia | awk '{print $2}')"
 sudo apt autoremove
