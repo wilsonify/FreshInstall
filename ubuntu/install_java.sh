54b0f9fab8328ff9bd8b8f03edd07a95bed6747c
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-12-30 15:13:03 -0500

    better export of JAVA_HOME

--- ubuntu/install_java.sh
+++ ubuntu/install_java.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 sudo apt-get install openjdk-8-jdk
-LINE='JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin"'
+JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin"
+export JAVA_HOME
+LINE="JAVA_HOME=$JAVA_HOME"
 FILE='/etc/environment'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE" #check for line in file. If missing, add it.
-source $FILE
-sudo R CMD javareconf 
+sudo R CMD javareconf
