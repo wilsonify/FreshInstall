4edc354f6e7dbb667e62ae11ccc9becff7e6b8ea
wilsonify <tom.a.wilson@gmail.com>
2021-02-21 10:31:54 -0500

    create jamovi installer

---
+++ ubuntu/install-jamovi.sh
@@ -0,0 +1,3 @@
+sudo apt install flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.jamovi.jamovi
