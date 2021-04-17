b1d174381807a2144a66b12ccfa5b723f9eea443
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-04-17 11:59:02 -0400

    better download extraction logic for smartgit

--- ubuntu/install-smartgit.sh
+++ ubuntu/install-smartgit.sh
@@ -1,24 +1,31 @@
 SMARTGIT_HOME=/opt/smartgit
+TAR_FILE=smartgit-linux-20_2_4.tar.gz
 
-echo "start downloading SmartGit"
-wget https://www.syntevo.com/downloads/smartgit/smartgit-linux-20_2_4.tar.gz
-echo "done downloading SmartGit"
-
-echo "start extracting SmartGit"
-tar -xzf "smartgit-linux-20_2_4.tar.gz"
-echo "done extracting SmartGit"
+if [[ -s $TAR_FILE ]]
+then
+  echo "$TAR_FILE found"
+else
+  echo "$TAR_FILE not found"
+  echo "start downloading SmartGit"
+  wget https://www.syntevo.com/downloads/smartgit/$TAR_FILE
+  echo "done downloading SmartGit"
+fi
 
 echo "start removing existing installation"
-rm -rf $SMARTGIT_HOME
+sudo rm -rf $SMARTGIT_HOME
 sudo mkdir $SMARTGIT_HOME
 echo "done removing existing installation"
 
+echo "start extracting SmartGit"
+sudo tar -xzf "$TAR_FILE" --directory=/opt
+echo "done extracting SmartGit"
+
 echo "start installing SmartGit"
-sudo mv smartgit-linux-20_2_4/* $SMARTGIT_HOME
 cd $SMARTGIT_HOME/bin
-remove-menuitem.sh
-smartgit.sh
-add-menuitem.sh
+pwd
+./remove-menuitem.sh
+./smartgit.sh
+./add-menuitem.sh
 export PATH=$PATH:$SMARTGIT_HOME/bin
 echo "done installing SmartGit"
 
