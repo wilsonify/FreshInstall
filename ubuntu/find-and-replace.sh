e8338a9a65d690384a7ab27fd7ea2d27fa038a76
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-05-02 20:37:55 -0400

    update for pycaret venv

--- ubuntu/find-and-replace.sh
+++ ubuntu/find-and-replace.sh
@@ -1,9 +1,9 @@
 # useful for salvaging a virtual environment
 
-cd /mnt/SSD1/venvs
+cd /mnt/SSD1/venvs/pycaret37/
 pwd
 OLD="\/mnt\/c8c20bab-7445-45b1-b260-86f1a92c7b20\/"
+OLD="\/home\/thom\/"
 NEW="\/mnt\/SSD1\/"
-
 echo "s/$OLD/$NEW/g"
 find . -type f -exec sed -i "s/$OLD/$NEW/g" {} +
