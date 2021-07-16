096d3d302250887c84a0128e10b4c0b8955ece06
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-07-16 08:27:59 -0400

    include PATH during install

--- ubuntu/tensorflow/nvidia_cuda_10.2_install.sh
+++ ubuntu/tensorflow/nvidia_cuda_10.2_install.sh
@@ -1,2 +1,7 @@
 wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
 sudo ./cuda_10.2.89_440.33.01_linux.run --toolkit
+
+# you probably want these in /etc/environment
+export PATH=$PATH:/usr/local/cuda/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
+
