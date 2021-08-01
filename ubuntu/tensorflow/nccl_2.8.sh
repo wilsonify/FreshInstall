b24c88b81a15d131dcfb0216c165852eaa340c81
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-08-01 13:15:28 -0400

    nccl for cuda 10.2

--- ubuntu/tensorflow/nccl_2.8.sh
+++ ubuntu/tensorflow/nccl_2.8.sh
@@ -1,8 +1,8 @@
-basepath=nccl_2.8.3-1+cuda11.1_x86_64
-txzfile=nccl_2.8.3-1+cuda11.1_x86_64.txz
+basepath=nccl_2.8.3-1+cuda10.2_x86_64
+txzfile=nccl_2.8.3-1+cuda10.2_x86_64.txz
 # wget https://developer.nvidia.com/compute/machine-learning/nccl/secure/2.8.3/agnostic/x64/$txzfile
 # tar -xfv $txzfile
-sudo cp $basepath/include/* /usr/local/cuda/include
-sudo cp $basepath/lib/* /usr/local/cuda/lib64
+sudo cp -r $basepath/include/* /usr/local/cuda/include
+sudo cp -r $basepath/lib/* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/nccl*.h
 sudo chmod a+r /usr/local/cuda/lib64/libnccl*
