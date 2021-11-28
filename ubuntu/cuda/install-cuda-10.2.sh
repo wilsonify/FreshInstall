5cd42005e8bb8caaf212a4c38d0680d61f658643
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-11-28 15:22:02 -0500

    explicit version number

---
+++ ubuntu/cuda/install-cuda-10.2.sh
@@ -0,0 +1,6 @@
+sudo sh /mnt/SSD2/nvidia-cuda/cuda-10.2/cuda_10.2.89_440.33.01_linux.run \
+--silent \
+--toolkit \
+--toolkitpath=/usr/local/cuda-10.2 \
+--samples \
+--samplespath=/usr/local/cuda-10.2/NVIDIA_CUDA-10.2_Samples
