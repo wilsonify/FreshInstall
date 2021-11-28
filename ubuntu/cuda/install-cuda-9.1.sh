52613ca4fb57b60d7fbeb9270eeec04c10f0375c
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-11-28 11:47:00 -0500

    create script for cuda-9.1 run file

---
+++ ubuntu/cuda/install-cuda-9.1.sh
@@ -0,0 +1,7 @@
+sudo sh /mnt/SSD2/nvidia-cuda/cuda-9.1/cuda_9.1.85_387.26_linux.run \
+--silent \
+--toolkit \
+--toolkitpath=/usr/local/cuda-9.1 \
+--samples \
+--samplespath=/usr/local/cuda-9.1/NVIDIA_CUDA-9.1_Samples \
+--verbose
