db69a84afe5490b030c39c550097e0b9c97a6753
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-11-28 11:42:31 -0500

    more complete alternative-gcc.sh

--- ubuntu/tensorflow/alternative-gcc.sh
+++ ubuntu/tensorflow/alternative-gcc.sh
@@ -1,10 +1,40 @@
-#sudo apt install gcc-7 g++-7
-#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 70 --slave /usr/bin/g++ g++ /usr/bin/g++-7 --slave /usr/bin/gcov gcov /usr/bin/gcov-7
 
+# add olo apt repo for old gcc
+sudo apt-add-repository 'deb http://us.archive.ubuntu.com/ubuntu/ bionic main universe'
+sudo apt install gcc-6 g++-6
+sudo apt install gcc-7 g++-7
 sudo apt install gcc-8 g++-8
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80 --slave /usr/bin/g++ g++ /usr/bin/g++-8 --slave /usr/bin/gcov gcov /usr/bin/gcov-8
+sudo apt install gcc-9 g++-9
+sudo apt install gcc-10 g++-10
+sudo apt install gcc-11 g++-11
 
-#sudo apt install gcc-9 g++-9
-#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 90 --slave /usr/bin/g++ g++ /usr/bin/g++-9 --slave /usr/bin/gcov gcov /usr/bin/gcov-9
+# erase exiting update-alternatives setup for gcc and g++:
+sudo update-alternatives --remove-all gcc
+sudo update-alternatives --remove-all g++
 
+# Install Alternatives
+# Symbolic links cc and c++ are installed by default.
+# 10, 20 and 30 options are the priorities for each alternative, bigger number is a higher priority.
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 20
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 30
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 40
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 50
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 30
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 40
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 50
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 60
+
+# Configure Alternatives
+# configuring the default commands for gcc, g++. It's easy to switch interactively:
 sudo update-alternatives --config gcc
+sudo update-alternatives --config g++
+
+# Add symlinks within CUDA folders:
+sudo ln -s /usr/bin/gcc-6 /usr/local/cuda-9.1/bin/gcc
+sudo ln -s /usr/bin/g++-6 /usr/local/cuda-9.1/bin/g++
+
