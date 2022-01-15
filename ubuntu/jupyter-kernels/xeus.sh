03719eee678b73ed76556484cd0a30a9cee226b3
Tom Wilson <tom.andrew.wilson@gmail.com>
2022-01-15 10:45:26 -0500

    capture install for xeus

---
+++ ubuntu/jupyter-kernels/xeus.sh
@@ -0,0 +1,15 @@
+
+# C++ For Jupyter Notebook
+# Xeus; a C++ kernel for Jupyter Notebook
+# Xeus-Cling; a C++ interpreter.
+# Together, these two frameworks allow for seamless interactive computing.
+
+# Requirements:
+# Mac OS/Linux/(Windows is still experimental at the time of this article)
+# Conda package manager
+
+install xeus-cling
+
+install xeus
+
+
