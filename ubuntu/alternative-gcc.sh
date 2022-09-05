c980c3be7693c3ed144dd67e58ecf43286de1132
wilsonify <tom.andrew.wilson@gmail.com>
2022-09-05 14:50:21 -0400

    changes for ubuntu 22.04 jammy-jellyfish

--- ubuntu/alternative-gcc.sh
+++ ubuntu/alternative-gcc.sh
@@ -1,10 +1,44 @@
 # sometimes, it is necessary to match gcc version with some pre-compiled library
-# add olo apt repo for old gcc
-sudo apt-add-repository 'deb http://us.archive.ubuntu.com/ubuntu/ bionic main universe'
-sudo apt install gcc-5 g++-5
-sudo apt install gcc-6 g++-6
-sudo apt install gcc-7 g++-7
-sudo apt install gcc-8 g++-8
+# add old apt repo for old gcc
+
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/gcc-8-base_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/libmpx2_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/lib32mpx2_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/libgcc-8-dev_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/gcc-8_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/g++-8-multilib_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/g++-8_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/libstdc++-8-dev_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/gcc-8-multilib_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/lib32stdc++-8-dev_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/libx32stdc++-8-dev_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/cpp-8_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/libx32gcc-8-dev_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gcc-8/lib32gcc-8-dev_8.4.0-3ubuntu2_amd64.deb
+wget http://mirrors.edge.kernel.org/ubuntu/pool/main/i/isl/libisl22_0.22.1-1_amd64.deb
+
+# sudo apt install gcc-8 g++-8
+sudo apt install ./gcc-8-base_8.4.0-3ubuntu2_amd64.deb
+sudo apt install ./libmpx2_8.4.0-3ubuntu2_amd64.deb
+sudo apt install ./lib32mpx2_8.4.0-3ubuntu2_amd64.deb
+sudo apt install \
+  ./libisl22_0.22.1-1_amd64.deb \
+  ./lib32gcc-8-dev_8.4.0-3ubuntu2_amd64.deb \
+  ./libx32gcc-8-dev_8.4.0-3ubuntu2_amd64.deb \
+  ./libstdc++-8-dev_8.4.0-3ubuntu2_amd64.deb \
+  ./gcc-8-multilib_8.4.0-3ubuntu2_amd64.deb \
+  ./lib32stdc++-8-dev_8.4.0-3ubuntu2_amd64.deb \
+  ./libx32stdc++-8-dev_8.4.0-3ubuntu2_amd64.deb \
+  ./cpp-8_8.4.0-3ubuntu2_amd64.deb \
+  ./libgcc-8-dev_8.4.0-3ubuntu2_amd64.deb \
+  ./gcc-8_8.4.0-3ubuntu2_amd64.deb \
+  ./g++-8_8.4.0-3ubuntu2_amd64.deb \
+  ./g++-8-multilib_8.4.0-3ubuntu2_amd64.deb
+
+#sudo apt install gcc-5 g++-5
+#sudo apt install gcc-6 g++-6
+#sudo apt install gcc-7 g++-7
+
 sudo apt install gcc-9 g++-9
 sudo apt install gcc-10 g++-10
 sudo apt install gcc-11 g++-11
@@ -17,16 +51,16 @@ sudo update-alternatives --remove-all g++
 # Symbolic links cc and c++ are installed by default.
 # bigger number is a higher priority.
 
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 10
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 20
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 30
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 10
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 20
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 30
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 40
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 50
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 70
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 10
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 20
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 30
+#sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 10
+#sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 20
+#sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 30
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 40
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 50
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 60
@@ -38,4 +72,3 @@ sudo update-alternatives --config gcc
 sudo update-alternatives --config g++
 
 # Add symlinks within CUDA folders:
-
--- ubuntu/nvidia/cuda/install-cuda-10.2.sh
+++ ubuntu/nvidia/cuda/install-cuda-10.2.sh
@@ -1,4 +1,4 @@
-sudo sh /mnt/SSD2/nvidia-cuda/cuda-10.2/cuda_10.2.89_440.33.01_linux.run \
+sudo sh /media/thom/seagate01/software/nvidia-cuda/cuda-10.2/cuda_10.2.89_440.33.01_linux.run \
 --silent \
 --toolkit \
 --toolkitpath=/usr/local/cuda-10.2 \
@@ -7,3 +7,28 @@ sudo sh /mnt/SSD2/nvidia-cuda/cuda-10.2/cuda_10.2.89_440.33.01_linux.run \
 
 sudo ln -s /usr/bin/gcc-8 /usr/local/cuda-10.2/bin/gcc
 sudo ln -s /usr/bin/g++-8 /usr/local/cuda-10.2/bin/g++
+
+# 13. Post-installation Actions
+cat /etc/environment | grep cuda-10
+export PATH=${PATH}:/usr/local/cuda-10.2/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-10.2/lib64
+
+# The post-installation actions must be manually performed.
+# These actions are split into mandatory, recommended, and optional sections.
+# 13.1. Mandatory Actions
+# Some actions must be taken after the installation before the CUDA Toolkit and Driver can be used.
+# 13.1.1. Environment Setup
+# The PATH variable needs to include
+# Nsight Compute has moved to /opt/nvidia/nsight-compute/ only in rpm/deb installation method.
+# When using .run installer it is still located under /usr/local/cuda-10.2/.
+# To add this path to the PATH variable:
+# export PATH=/usr/local/cuda-10.2/bin${PATH:+:${PATH}}
+# In addition, when using the runfile installation method,
+# the LD_LIBRARY_PATH variable needs to contain /usr/local/cuda-10.2/lib64 on a 64-bit system,
+# or /usr/local/cuda-10.2/lib on a 32-bit system
+# To change the environment variables for 64-bit operating systems:
+# export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+# To change the environment variables for 32-bit operating systems:
+# export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+# Note that the above paths change when using a custom install path with the runfile installation method.
+# You will need to reboot the system to initialize the above changes.
