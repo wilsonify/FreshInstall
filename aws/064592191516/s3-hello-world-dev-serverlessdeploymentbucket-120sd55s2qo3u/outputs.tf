5df4b07f7791cc2f94356e7dee43808b1800e1b5
wilsonify <tom.a.wilson@gmail.com>
2023-08-25 07:59:35 -0400

    export more buckets

---
+++ aws/064592191516/s3-hello-world-dev-serverlessdeploymentbucket-120sd55s2qo3u/outputs.tf
@@ -0,0 +1,7 @@
+output "aws_s3_bucket_policy_tfer--hello-world-dev-serverlessdeploymentbucket-120sd55s2qo3u_id" {
+  value = "${aws_s3_bucket_policy.tfer--hello-world-dev-serverlessdeploymentbucket-120sd55s2qo3u.id}"
+}
+
+output "aws_s3_bucket_tfer--hello-world-dev-serverlessdeploymentbucket-120sd55s2qo3u_id" {
+  value = "${aws_s3_bucket.tfer--hello-world-dev-serverlessdeploymentbucket-120sd55s2qo3u.id}"
+}
