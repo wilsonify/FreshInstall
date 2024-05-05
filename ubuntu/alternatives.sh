7e2acc96ce0ad9f4391020b1b2aee3864ff96906
wilsonify <tom.andrew.wilson@gmail.com>
2024-05-05 14:14:43 -0400

    alternatives.sh

--- ubuntu/alternatives.sh
+++ ubuntu/alternatives.sh
@@ -1,15 +1,59 @@
-sudo update-alternatives --remove-all aws && sudo update-alternatives --install /usr/bin/aws aws /opt/aws/aws-cli/v2.13.11/aws 100
-sudo update-alternatives --remove-all bazel && sudo update-alternatives --install /usr/bin/bazel bazel /opt/bazel/bazelisk 100
-sudo update-alternatives --remove-all code && sudo update-alternatives --install /usr/bin/code code /opt/vscode/v1.84.0/bin/code 100
-sudo update-alternatives --remove-all firefox && sudo update-alternatives --install /usr/bin/firefox firefox /opt/firefox/firefox 100
-sudo update-alternatives --remove-all goofys && sudo update-alternatives --install /usr/bin/goofys goofys /opt/goofys/v0.24.0/goofys 100
-sudo update-alternatives --remove-all hugo && sudo update-alternatives --install /usr/bin/hugo hugo /opt/hugo/hugo 100
-sudo update-alternatives --remove-all idea && sudo update-alternatives --install /usr/bin/idea idea /opt/idea/2023.3/bin/idea.sh 100
-sudo update-alternatives --remove-all julia && sudo update-alternatives --install /usr/bin/julia julia /opt/julia/v1.5.4/bin/julia 100
-sudo update-alternatives --remove-all pnpm && sudo update-alternatives --install /usr/bin/pnpm pnpm /opt/pnpm/bin/pnpm-linux-x64 100
-sudo update-alternatives --remove-all pycharm && sudo update-alternatives --install /usr/bin/pycharm pycharm /opt/pycharm-community/2023.2.1/bin/pycharm.sh 100
-sudo update-alternatives --remove-all rclone && sudo update-alternatives --install /usr/bin/rclone rclone /opt/rclone/v1.65.2/rclone 100
-sudo update-alternatives --remove-all saml2aws && sudo update-alternatives --install /usr/bin/saml2aws saml2aws /opt/saml2aws/2.36.12/saml2aws 100
-sudo update-alternatives --remove-all terraform && sudo update-alternatives --install /usr/bin/terraform terraform /opt/terraform/v1.5.7/terraform 100
-sudo update-alternatives --remove-all terraformer && sudo update-alternatives --install /usr/bin/terraformer terraformer /opt/terraformer/v0.8.24/terraformer 100
-sudo update-alternatives --remove-all vscode && sudo update-alternatives --install /usr/bin/vscode vscode /opt/vscode/bin/code 100
+#!/bin/bash
+
+handle_error() {
+  local message="$1"
+  echo "handle errors"
+  echo "Error: $message" >&2
+  exit 1
+}
+
+remove_alternatives() {
+  local name="$1"
+  echo "remove existing alternatives for $name"
+  if ! sudo update-alternatives --remove-all "$name"; then
+    handle_error "Failed to remove existing alternatives for $name"
+  fi
+}
+
+install_alternatives() {
+  local name="$1"
+  local target="$2"
+  local priority="$3"
+  echo "install new alternative for $name"
+  if ! sudo update-alternatives --install "/usr/bin/$name" "$name" "$target" "$priority"; then
+    handle_error "Failed to install alternative for $name"
+  fi
+}
+
+main() {
+  # List of commands to update alternatives
+  local commands=(
+    "aws /opt/aws/aws-cli/v2.13.11/aws 100"
+    "bazel /opt/bazel/bazelisk 100"
+    "code /opt/vscode/v1.84.0/bin/code 100"
+    "firefox /opt/firefox/firefox 100"
+    "goofys /opt/goofys/v0.24.0/goofys 100"
+    "hugo /opt/hugo/hugo 100"
+    "idea /opt/idea/2023.3/bin/idea.sh 100"
+    "julia /opt/julia/v1.5.4/bin/julia 100"
+    "pnpm /opt/pnpm/bin/pnpm-linux-x64 100"
+    "pycharm /opt/pycharm-community/2023.2.1/bin/pycharm.sh 100"
+    "rclone /opt/rclone/v1.65.2/rclone 100"
+    "saml2aws /opt/saml2aws/2.36.12/saml2aws 100"
+    "terraform /opt/terraform/v1.5.7/terraform 100"
+    "terraformer /opt/terraformer/v0.8.24/terraformer 100"
+    "vscode /opt/vscode/bin/code 100"
+  )
+
+  echo "start updating alternatives"
+  # Loop through each command and update alternatives
+  for command in "${commands[@]}"; do
+    read -r name target priority <<<"$command"
+    remove_alternatives "$name"
+    install_alternatives "$name" "$target" "$priority"
+  done
+  echo "All alternatives updated successfully."
+}
+
+# Call the main function
+main
