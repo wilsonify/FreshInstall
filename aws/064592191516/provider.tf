71cd725528a58708cfbabc8b6f346089ebd74cac
wilsonify <tom.andrew.wilson@gmail.com>
2024-09-08 19:08:07 -0400

    provider.tf

--- aws/064592191516/provider.tf
+++ aws/064592191516/provider.tf
@@ -1,11 +1,10 @@
-provider "aws" {}
-
 terraform {
   backend "s3" {
-    bucket = "064592191516-terraform-state"
-    key = "terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "064592191516-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "064592191516-lock-table"
+    profile        = "064592191516"
   }
   required_providers {
     aws = {
